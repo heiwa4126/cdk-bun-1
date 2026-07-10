062dab040520fb6315ec416c5e9c3792c21c7bd6
heiwa4126 <heiwa4126@gmail.com>
2026-07-10 11:15:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# cdk-bun-1
+
+AWS CDK を全部 bun でやるテスト。
+
+TypeScript, ts-node, jest なし (esbuild あり)
+
+```sh
+bun ci
+
+# ローカルで
+bun run list
+bun run synth
+
+# デプロイ
+aws login
+bun run deploy
+
+# おわったら消す
+bun run destroy
+```
---
+++ test/cdk-bun-1.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBun1 from '../lib/cdk-bun-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-bun-1-stack.ts
+test("SQS Queue Created", () => {
+	//   const app = new cdk.App();
+	//     // WHEN
+	//   const stack = new CdkBun1.CdkBun1Stack(app, 'MyTestStack');
+	//     // THEN
+	//   const template = Template.fromStack(stack);
+	//   template.hasResourceProperties('AWS::SQS::Queue', {
+	//     VisibilityTimeout: 300
+	//   });
+});
